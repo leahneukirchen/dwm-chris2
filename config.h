4508b0013716db6e323bca8c3a7cfb6d2078757c
Christian Neukirchen <chneukirchen@gmail.com>
2012-08-25 17:16:58 +0200

    togglefullscreen.patch

--- config.h
+++ config.h
@@ -53,8 +53,10 @@ static const Layout layouts[] = {
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "uxterm", NULL };
 
+#include "togglefullscreen.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+        { MODKEY,                       XK_x,      togglefullscreen, {0} },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
