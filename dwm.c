a44aa4c8899f0de351d0219dadb18dcaa382f685
Christian Neukirchen <chneukirchen@gmail.com>
2012-08-25 17:22:14 +0200

    12px top border

--- dwm.c
+++ dwm.c
@@ -1925,9 +1925,9 @@ updategeom(void) {
 					dirty = True;
 					m->num = i;
 					m->mx = m->wx = unique[i].x_org;
-					m->my = m->wy = unique[i].y_org;
+					m->my = m->wy = unique[i].y_org + 12;
 					m->mw = m->ww = unique[i].width;
-					m->mh = m->wh = unique[i].height;
+					m->mh = m->wh = unique[i].height - 12;
 					updatebarpos(m);
 				}
 		}
